653b62157c083e092316fbed1f0845bd6a06f98c
Ragiinee <offaof2002@gmail.com>
2025-05-12 14:07:53 +0700

    Ch2_edit_1

--- src/Charter_2.jsx
+++ src/Charter_2.jsx
@@ -5,7 +5,7 @@ const theory = [
         id: 1,
         front: [{
             name_front: "การซื้อขาย ",
-            img: "https://scontent.fbkk29-1.fna.fbcdn.net/v/t1.15752-9/494829263_590489643443685_7603448704682353184_n.jpg?stp=dst-jpg_s480x480_tt6&_nc_cat=100&ccb=1-7&_nc_sid=0024fc&_nc_ohc=VAHgU0VVpnAQ7kNvwH6mf2_&_nc_oc=Admk7l4UnO3SNxOAN6I1Ew0T2gQf9V297JGBm0Ip50nDemsBWd6mUCx9JPpCY13VW1owp679XZU8zsLXMlEa7S4v&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent.fbkk29-1.fna&oh=03_Q7cD2QE2AkCwDocKCUx-4GiPw6TnVb4P8Ti18J0knuRlv3l1EA&oe=68483B25"
+            img: "https://static.tradingmoon.com/static/assets/images/tradingmoon-logo.svg"
         }],
         back: [{
             name_back: "การซื้อขายในตลาดการเงินคือ การแลกเปลี่ยนสินทรัพย์ (หุ้น, พันธบัตร, ฯลฯ) เพื่อ ทำกำไรจากราคาขึ้นลง ทั้งระยะสั้นและยาว ตลาดมีเครื่องมือเยอะ พัฒนาสู่ระบบออนไลน์ที่เร็วมาก การซื้อขาย เน้นกำไรเร็ว เสี่ยงสูง ต่างจากการลงทุนระยะยาว การเข้าใจพื้นฐานจึงสำคัญ",
@@ -16,7 +16,7 @@ const theory = [
         id: 2,
         front: [{
             name_front: "แนวคิดเกี่ยวกับธุรกิจร้านขายอาหารสัตว์",
-            img: "https://scontent.fbkk29-4.fna.fbcdn.net/v/t1.15752-9/494830802_1113838653837912_8605502957760499554_n.jpg?stp=dst-jpg_s480x480_tt6&_nc_cat=111&ccb=1-7&_nc_sid=0024fc&_nc_ohc=WyNO47WLyIsQ7kNvwG7djsP&_nc_oc=Adnn9DvOOkMhqeqsfIy62uI4qAjpnxLOlX_874yAbkairNMJ3VXLLa6i0Lm1M96DqVzqZSP_xIJha-X-CO4ma-Nw&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent.fbkk29-4.fna&oh=03_Q7cD2QH9ZI2S24uDWuLeAQZkrCG8qVKynvlyH2gyCKGRU3rrNg&oe=684845EE"
+            img: "https://scontent.fbkk29-9.fna.fbcdn.net/v/t39.30808-6/495185903_1169171971672176_7800378232011813624_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=9dTxw9n_d0cQ7kNvwGIYV49&_nc_oc=AdlxgQ_NCREXE5Nl5WtjEaXZNh1QfHl7ODxRT9iBo2vBrIbR0J9iaRFXXGa8pQ2vZZ20bBg5ln7tD_K65MeOQKJ2&_nc_zt=23&_nc_ht=scontent.fbkk29-9.fna&_nc_gid=cW-lmrcEk9G8aqCyXW-lig&oh=00_AfJT8WhO7kze91zrvsJKQkSVlN1Qcja-Iv9WAwcW3WMaug&oe=68275AAF"
         }],
         back: [{
             name_back: "ธุรกิจเกี่ยวกับสัตว์เลี้ยงในปัจจุบันกำลังเติบโตอย่างมากในประเทศไทย สาเหตุหลักมาจากโครงสร้างครอบครัวที่เปลี่ยนไป คนนิยมเลี้ยงสัตว์เป็นเพื่อนมากขึ้น ทำให้ความต้องการสินค้าและบริการสำหรับสัตว์เลี้ยงเพิ่มสูงขึ้นมาก ธุรกิจนี้มีการแข่งขันสูง มีสินค้าใหม่ๆ ออกมาตลอดเวลา ผู้ประกอบการที่ประสบความสำเร็จต้องมีความรู้ อัปเดตข้อมูล และสามารถให้คำแนะนำที่ดีแก่ลูกค้าได้",
@@ -27,7 +27,7 @@ const theory = [
         id: 3,
         front: [{
             name_front: "การจัดการสต็อก",
-            img: "https://scontent.fbkk29-6.fna.fbcdn.net/v/t1.15752-9/494832882_9428444183945133_3194214865115452997_n.jpg?stp=dst-jpg_s480x480_tt6&_nc_cat=103&ccb=1-7&_nc_sid=0024fc&_nc_ohc=F1sH4SEVprgQ7kNvwFfU5LX&_nc_oc=AdkYQz-Gg7Wehdy-bmCr4wvRjDy12oJJJUA-fte1AfwJDHrLDR7YhmqFCa71RW1Bm5jXdBxmiyUi0zRqTj1erMTL&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent.fbkk29-6.fna&oh=03_Q7cD2QG8_F2-t9Zf3EunLswjNUQ4mJ3aN8HuCdZ047QzcdYfQQ&oe=6848345F"
+            img: "./img/DRTANITSORAT-removebg-preview.png"
         }],
         back: [{
             name_back: "คลังสินค้าคือที่เก็บรักษาสินค้าให้พร้อมส่งมอบ มีการจัดเก็บ 2 แบบคือ แบบสุ่ม (เข้าก่อนออกก่อน เน้นใช้พื้นที่) และแบบกำหนดตำแหน่ง (ระบุที่เก็บตายตัว) ซึ่งปัจจุบันมีการใช้เทคโนโลยีอัตโนมัติช่วย โดยการจัดการสต็อกที่ดีช่วยเพิ่มกำไร ป้องกันสินค้าสูญหาย ลดค่าใช้จ่ายสินค้าค้างสต็อก และช่วยวางแผนการระบายสินค้า",
@@ -38,7 +38,7 @@ const theory = [
         id: 4,
         front: [{
             name_front: "ฐานข้อมูลคลังสินค้า",
-            img: "https://scontent.fbkk29-8.fna.fbcdn.net/v/t1.15752-9/489729884_988005596868961_8978320094380456645_n.jpg?stp=dst-jpg_s480x480_tt6&_nc_cat=105&ccb=1-7&_nc_sid=0024fc&_nc_ohc=Nh8LcQWpWLAQ7kNvwHg4BCY&_nc_oc=AdlFeaeUGGmeCsMplnwxXdaNP8Toyyb2cS9p8Ph0pwDRBtlKtZLBtA1cjl6R0OxXDzpMsuTlzhhaDz7iP_oINyuN&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent.fbkk29-8.fna&oh=03_Q7cD2QFXoJ3chSTsj_ja58OhjcaWu_5iNB1ynXipUKTjVUonRg&oe=6848471C"
+            img: "https://www.mindphp.com/images/info/mindphp-logo-v40.png"
         }],
         back: [{
             name_back: "คลังข้อมูลสำคัญเพราะช่วยให้เข้าใจสถานการณ์และตัดสินใจได้ดีขึ้น มีประโยชน์คือ เสริมความรู้ สนับสนุนการตัดสินใจ เข้าถึงข้อมูลเร็ว แยกฐานข้อมูลปฏิบัติงานกับวิเคราะห์ ให้ข้อมูลสรุปและเจาะลึกได้ โดยคลังข้อมูลเป็นระบบจัดการข้อมูลที่ออกแบบมาเพื่อสนับสนุนการวิเคราะห์ข้อมูลทางธุรกิจ โดยรวบรวมข้อมูลจากหลายแหล่งในอดีต เพื่อช่วยในการตัดสินใจ",
@@ -49,18 +49,18 @@ const theory = [
         id: 5,
         front: [{
             name_front: "พฤติกรรมผู้บริโภค",
-            img: "https://scontent.fbkk29-7.fna.fbcdn.net/v/t1.15752-9/494834987_591623463331092_176204472186910812_n.jpg?stp=dst-jpg_s480x480_tt6&_nc_cat=106&ccb=1-7&_nc_sid=0024fc&_nc_ohc=IRqbFc5fIjkQ7kNvwFFc-Hu&_nc_oc=Adn68-WChjJIxuqiMEAuysEXYWDkSy-OOmOf1AthLFXgUXJWQdA93AVaHdTjua8wjdGadhAOQwC6h_fVdecw-Gpn&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent.fbkk29-7.fna&oh=03_Q7cD2QG8ApQ1ffSeL-CRIQYoJG8Kz6syZ9mJWLC3wXKgoSNvTA&oe=68485352"
+            img: "https://ia800505.us.archive.org/view_archive.php?archive=/25/items/m_covers_0010/m_covers_0010_45.zip&file=0010450120-M.jpg"
         }],
         back: [{
             name_back: "พฤติกรรมผู้บริโภคคือการศึกษาว่าคน เลือก ซื้อ ใช้ และทิ้ง สินค้า/บริการ เพื่อตอบสนอง ความจำเป็นและความต้องการ โดยมีขั้นตอน 5 อย่าง 1) รับรู้ปัญหา: รู้ว่าต้องการอะไร 2) หาข้อมูล: ค้นหาสินค้าที่ต้องการ 3) เปรียบเทียบ: เลือกระหว่างยี่ห้อที่รู้จัก 4) ตัดสินใจซื้อ: เลือกซื้อสินค้าที่ชอบ 5)หลังซื้อ: ประเมินว่าพอใจหรือไม่",
-            link: "#Charter2"
+            link: "https://openlibrary.org/books/OL30334377M/Marketing_Management_14th_Ed._By_Philip_Kotler"
         }]
     },
     {
         id: 6,
         front: [{
             name_front: "งานวิจัยเรื่องการพัฒนาเว็บไซต์เพื่อจำหน่ายอาหารสัตว์เลี้ยง: กรณีศึกษาร้าน Petshop ",
-            img: "https://scontent.fbkk29-7.fna.fbcdn.net/v/t1.15752-9/489730736_1615628005737814_1394898887532754045_n.jpg?stp=dst-jpg_s480x480_tt6&_nc_cat=106&ccb=1-7&_nc_sid=0024fc&_nc_ohc=bySip-pBI_EQ7kNvwGQpmLw&_nc_oc=Adma-PnO_CeXlORJq9MYfeZerPRztG5xffnYnJLe2ZA1n5fUlpGHyosB9gAJ6gTCIu6FD1aM-KjGjdtcDCBtG-cj&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent.fbkk29-7.fna&oh=03_Q7cD2QGgPphSYJ-oAQnRXn7UFhhmqUiO6Zpb4h6Ycfugx9jYBQ&oe=6848581C"
+            img: "https://th.bing.com/th/id/OIP._1U8pL65y7TFnOqJzzsCIAHaHa?cb=iwc2&rs=1&pid=ImgDetMain"
         }],
         back: [{
             name_back: "พัฒนาเว็บไซต์ขายอาหารสัตว์เลี้ยงเพื่อลดต้นทุนค่าเช่าและเพิ่มช่องทางขาย โดยมีเป้าหมายเพื่อให้ลูกค้าซื้อสินค้าได้สะดวกขึ้นและลดค่าใช้จ่ายของร้าน  วิธีการคือสร้างเว็บไซต์ที่ลูกค้าสมัครสมาชิก ค้นหาสินค้า สั่งซื้อ และชำระเงินได้ โดยออกแบบให้ใช้งานได้ดีบนทุกอุปกรณ์ สรุปคือ ร้านทำเว็บไซต์ให้ลูกค้าซื้อของง่ายขึ้นและประหยัดค่าเช่าร้าน",
@@ -71,10 +71,10 @@ const theory = [
         id: 7,
         front: [{
             name_front: "งานวิจัยเรื่องการพัฒนาบริการซอฟต์แวร์คลาวด์สำหรับการบริหารจัดการร้านขายอาหารสัตว์ กรณีศึกษาร้านอู๊ดดี้เพ็ทช็อป",
-            img: "https://scontent.fbkk29-5.fna.fbcdn.net/v/t1.15752-9/489730768_1189203602945354_1946060852299376563_n.jpg?stp=dst-jpg_s480x480_tt6&_nc_cat=107&ccb=1-7&_nc_sid=0024fc&_nc_ohc=7tZ4oK-9aKAQ7kNvwFLBjR7&_nc_oc=AdmcVnYoIq0yC4WY-QaVshLuRW_ocBVQEn_SfykBKRXp2JLag6P7f8k2rBJO841IslBgYpElx2cv5whOuMzkPMDU&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent.fbkk29-5.fna&oh=03_Q7cD2QGd5s51gP0WQ34SHY0ONG6DgqZas8TVJ8MJlIcOJKYOtQ&oe=6848635D"
+            img: "https://scontent.fbkk29-9.fna.fbcdn.net/v/t39.30808-6/495185903_1169171971672176_7800378232011813624_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=9dTxw9n_d0cQ7kNvwGIYV49&_nc_oc=AdlxgQ_NCREXE5Nl5WtjEaXZNh1QfHl7ODxRT9iBo2vBrIbR0J9iaRFXXGa8pQ2vZZ20bBg5ln7tD_K65MeOQKJ2&_nc_zt=23&_nc_ht=scontent.fbkk29-9.fna&_nc_gid=cW-lmrcEk9G8aqCyXW-lig&oh=00_AfJT8WhO7kze91zrvsJKQkSVlN1Qcja-Iv9WAwcW3WMaug&oe=68275AAF"
         }],
         back: [{
-            name_back: "ร้านอู๊ดดี้เพ็ทช็อปได้พัฒนาซอฟต์แวร์คลาวด์ใหม่สำหรับบริหารจัดการร้าน  เพื่อให้การทำงานของร้านมีประสิทธิภาพมากขึ้น เช่น การบริการลูกค้า การจัดการสต็อกสินค้า และการดูข้อมูลเก่าๆ ได้ง่ายขึ้น โดยใช้ภาษา VB.NET ในการออกแบบและพัฒนา เริ่มจากการวิเคราะห์ ออกแบบระบบและฐานข้อมูล แล้วจึงสร้างโปรแกรม สรุปคือ ร้านสร้างโปรแกรมบนอินเทอร์เน็ตให้จัดการร้านได้ดีขึ้นและเข้าถึงข้อมูลได้ทุกที่",
+            name_back: "พัฒนาซอฟต์แวร์คลาวด์ใหม่สำหรับบริหารจัดการร้าน เพื่อให้การทำงานของร้านมีประสิทธิภาพมากขึ้น เช่น การบริการลูกค้า การจัดการสต็อกสินค้า และการดูข้อมูลเก่าๆ ได้ง่ายขึ้น โดยใช้ภาษา VB.NET ในการออกแบบและพัฒนา เริ่มจากการวิเคราะห์ ออกแบบระบบและฐานข้อมูล แล้วจึงสร้างโปรแกรม สรุปคือ ร้านสร้างโปรแกรมบนอินเทอร์เน็ตให้จัดการร้านได้ดีขึ้นและเข้าถึงข้อมูลได้ทุกที่",
             link: "https://dspace.spu.ac.th/server/api/core/bitstreams/cf9bc9b6-4e37-4962-9e7a-dc3a3709941b/content"
         }]
     },
@@ -82,7 +82,7 @@ const theory = [
         id: 8,
         front: [{
             name_front: "งานวิจัยเรื่องการออกแบบและพัฒนาแอปพลิเคชันระบบจัดการออเดอร์และสต็อกสินค้ากรณีศึกษา Penguintory",
-            img: "https://scontent.fbkk29-4.fna.fbcdn.net/v/t1.15752-9/489730947_1424185782330240_8312818649576371643_n.jpg?stp=dst-jpg_s480x480_tt6&_nc_cat=111&ccb=1-7&_nc_sid=0024fc&_nc_ohc=nLwcmI-wqNAQ7kNvwGBeDaS&_nc_oc=Adks17zxeFQceHAvcHh5q62JJxQxOxIkYey3eYSLAGWntHlH_1bQfHil1arcrin2gylGoFHfcdD61_Ny4mdqimX3&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent.fbkk29-4.fna&oh=03_Q7cD2QGmuEBJiDIb_dI-zYejOCit8NX-GNLF5bP8YikIQtwUNg&oe=68484B54"
+            img: "https://th.bing.com/th/id/R.a4cfcd3f85ab2e085750275c0ebf45a4?rik=d%2bJ3i3YHAnSBQw&riu=http%3a%2f%2fpws.npru.ac.th%2fkanet%2fdata%2fimages%2flogo_swu.gif&ehk=vN1Z%2bKm0vs%2fvxHgkTXi8rc1PsWQFx3aPKr50WMjJ4Bs%3d&risl=&pid=ImgRaw&r=0"
         }],
         back: [{
             name_back: "สร้างแอปพลิเคชันสำหรับจัดการออเดอร์และสินค้าคงคลัง เพื่อช่วยให้ผู้ประกอบการรายย่อยจัดการร้านได้ง่ายและมีประสิทธิภาพขึ้น  โดยใช้ขั้นตอน UX/UI เริ่มจากการหาข้อมูล ออกแบบตัวอย่างแอป ทดลองใช้กับกลุ่มเล็กๆ และนำข้อเสนอแนะมาปรับปรุง สรุปคือ สร้างแอปให้จัดการออเดอร์และสต็อกได้เร็วและดีขึ้น",
@@ -93,10 +93,10 @@ const theory = [
         id: 9,
         front: [{
             name_front: "งานวิจัยเรื่องการเพิ่มประสิทธิภาพในการจัดสต็อกสินค้าของบริษัท สแกนเนอร์ อิเลคทริค อินเตอร์เนชั่นแนล จำกัด",
-            img: "https://scontent.fbkk29-6.fna.fbcdn.net/v/t1.15752-9/494830877_1001519501970130_5208414614052629727_n.jpg?stp=dst-jpg_s480x480_tt6&_nc_cat=103&ccb=1-7&_nc_sid=0024fc&_nc_ohc=pruUPAqST2IQ7kNvwFvMo7s&_nc_oc=AdllL8dIA88MFhOgL2PrDskRGQMHCOVEZCcZQbr4q9Yrqx8MMam8YJc_pZXWvYhT-NVO7YD4a0KkvBqJEzdcGMSZ&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent.fbkk29-6.fna&oh=03_Q7cD2QFrnvHtCxC4nU87MZUc2gxzsJDoMwtMHuAfRTqFbfmTgA&oe=68484BCE"
+            img: "https://li01.tci-thaijo.org/public/journals/200/pageHeaderLogoImage_en_US.png"
         }],
         back: [{
-            name_back: "มีการศึกษาปัญหาการจัดการสต็อกสินค้าและแก้ไขโดยการทำป้ายรหัสสินค้า เพื่อให้การเบิกจ่ายสินค้ารวดเร็วขึ้น ลดขั้นตอน และจัดเก็บสินค้าให้เป็นระเบียบ โดยใช้วิธีวิเคราะห์ปัญหาและสร้างป้ายรหัสสินค้าติดที่สินค้า  สรุปคือ ศึกษาปัญหาแล้วทำป้ายรหัสสินค้าเพื่อให้จัดการสต็อกได้ง่ายและเร็วขึ้น",
+            name_back: "ศึกษาปัญหาการจัดการสต็อกสินค้าและแก้ไขโดยการทำป้ายรหัสสินค้า เพื่อให้การเบิกจ่ายสินค้ารวดเร็วขึ้น ลดขั้นตอน และจัดเก็บสินค้าให้เป็นระเบียบ โดยใช้วิธีวิเคราะห์ปัญหาและสร้างป้ายรหัสสินค้าติดที่สินค้า  สรุปคือ ศึกษาปัญหาแล้วทำป้ายรหัสสินค้าเพื่อให้จัดการสต็อกได้ง่ายและเร็วขึ้น",
             link: "https://e-research.siam.edu/wp-content/uploads/2019/04/business-management-2017-coop-Stock-Management-Performance-Improvement-of-Scaner-Electric-International-Co.-Ltd..pdf"
         }]
     },
@@ -104,7 +104,7 @@ const theory = [
         id: 10,
         front: [{
             name_front: "งานวิจัยเรื่องการพัฒนาระบบการจัดการสินค้าคงคลัง กรณีศึกษาบริษัทติดตั้งและบำรุงรักษา",
-            img: "https://scontent.fbkk29-8.fna.fbcdn.net/v/t1.15752-9/489730235_1095305812418875_3612480030361136435_n.jpg?stp=dst-jpg_s480x480_tt6&_nc_cat=102&ccb=1-7&_nc_sid=0024fc&_nc_ohc=Ui-wQVVp_CgQ7kNvwFQxXG1&_nc_oc=AdkT7mkmK0OP_rCbf0YhF0P5-N4undqcsXBaVx50U3ugGBrFbaojZaQUgUzDuva5bPHl76Xsl6AN5PQqx2ROPHIL&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent.fbkk29-8.fna&oh=03_Q7cD2QH87TqJpVgt7uM14TSIe7Mb5WtzDYLuD_1HJgKO24yILQ&oe=68485157"
+            img: "https://cdn.onefence.co/images/2022-05-27/8449d799-788a-4e04-8c09-2936191d4981.jpeg"
         }],
         back: [{
             name_back: "ศึกษาการจัดการสินค้าคงคลังโดยใช้ทฤษฎี ABC Analysis และ EOQ พร้อมทั้งทำใบ Stock card เพื่อให้บริหารจัดการสินค้าคงคลังได้ดีขึ้น ลดการสั่งซื้อซ้ำซ้อน และลดต้นทุนสินค้าค้างสต็อก โดยใช้วิธีวิเคราะห์และทำใบ Stock card ร่วมกับการใช้ทฤษฎี ABC Analysis และ EOQ สรุปคือ ศึกษาการจัดการสต็อกโดยใช้ทฤษฎีและทำเอกสารเพื่อให้จัดการสินค้าได้มีประสิทธิภาพและประหยัดต้นทุนมากขึ้น",
@@ -129,7 +129,7 @@ const theory = [
             img: "https://www.pikpng.com/pngl/b/446-4460428_electron-js-logo-png-clipart.png"
         }],
         back: [{
-            name_back: "Electron คือ กรอบงานที่ช่วยสร้างแอปพลิเคชันเดสก์ท็อป โดยใช้เทคโนโลยีเว็บที่คุณคุ้นเคยอย่าง JavaScript, HTML และ CSS มันรวมเอา Chromium (เบื้องหลังเบราว์เซอร์ Chrome) และ Node.js เข้าไว้ด้วยกัน ทำให้คุณสามารถ เขียนโค้ด JavaScript เพียงชุดเดียว และนำไปสร้างแอปพลิเคชันที่ทำงานได้บน Windows, macOS และ Linux โดยที่ไม่จำเป็นต้องมีความรู้ในการพัฒนาระบบปฏิบัติการโดยตรง",
+            name_back: "Electron Js คือ กรอบงานที่ช่วยสร้างแอปพลิเคชันเดสก์ท็อป โดยใช้เทคโนโลยีเว็บที่คุณคุ้นเคยอย่าง JavaScript, HTML และ CSS มันรวมเอา Chromium (เบื้องหลังเบราว์เซอร์ Chrome) และ Node.js เข้าไว้ด้วยกัน ทำให้คุณสามารถ เขียนโค้ด JavaScript เพียงชุดเดียว และนำไปสร้างแอปพลิเคชันที่ทำงานได้บน Windows, macOS และ Linux โดยที่ไม่จำเป็นต้องมีความรู้ในการพัฒนาระบบปฏิบัติการโดยตรง",
             link: "https://www.electronjs.org/docs/latest"
         }]
     },
@@ -184,8 +184,8 @@ export function Square() {
                                             className="top-0 right-0 bottom-0 left-0 absolute flex flex-col justify-between bg-white p-10 rounded-xl w-full h-full text-center cursor-pointer card__front"
                                         >
                                             <div className='flex justify-center items-center w-full h-full'>
-                                                <div className='w-50 h-50'>
-                                                    <img src={detail.img} alt="front_image" className='flex justify-center items-center rounded-full w-full h-full' />
+                                                <div className='flex justify-center items-center w-50 h-50'>
+                                                    <img src={detail.img} alt="front_image" className='pt-12 w-full h-full' />
                                                 </div>
                                             </div>
                                             <p className="font-bold text-black text-lg">{detail.name_front}</p>
@@ -199,7 +199,7 @@ export function Square() {
                                             <h2 className="text-black text-md text-justify">{detail.name_back}</h2>
                                             <a href={detail.link} className='flex justify-center items-center'>
                                                 <button className="self-center bg-white hover:bg-[#ffae93] hover:shadow-inner mt-auto px-4 py-2 rounded-full font-semibold text-amber-800 hover:text-white cursor-pointer">
-                                                    {detail.link !== "#Charter2" ? "ข้อมูลเพิ่มเติม" : "ศึกษาจากห้องสมุด"}
+                                                    {items.id !== 5 ? "ข้อมูลเพิ่มเติม" : "ศึกษาจากห้องสมุด"}
                                                 </button>
                                             </a>
                                         </div>
