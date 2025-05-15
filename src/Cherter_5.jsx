826382f2306b2ad7d81e4a09bfce100c24112eeb
Ragiinee <offaof2002@gmail.com>
2025-05-15 15:40:30 +0700

    CH_5_start

--- src/Cherter_5.jsx
+++ src/Cherter_5.jsx
@@ -2,7 +2,7 @@ export function Layouy_CH5() {
 
     return (
         <div className="bg-gray-50 w-full h-full md:h-[500px]">
-            บท 5
+            <h1 className="font-black text-5xl">บทสรุปและข้อเสนอแนะ</h1>
         </div>
     )
 }
@@ -10,7 +10,7 @@ export function Layouy_CH5() {
 
 export default function Charter_5() {
     return (
-        <div>
+        <div id="Charter_5" className="pt-36 md:pt-30 pb-18">
             <Layouy_CH5 />
         </div>
     )
